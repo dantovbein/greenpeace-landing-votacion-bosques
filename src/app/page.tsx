3e0f253d4a57e53aa1b54e8be2dfdf96cd6249fb
Dan Tovbein <dtovbein@gmail.com>
2023-07-04 09:06:32 -0300

    Implement preloader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import QuizForm from '@/app/_components/QuizForm'
 import { useAppContext } from './_contexts/app'
 import { useEffect } from 'react';
 import styles from '@/app/page.module.css'
+import Image from 'next/image';
 
 export default function Home() {
   const { fetchVotes, quiz: {yesVotes, noVotes, totalVotes}, fetched } = useAppContext();
@@ -16,9 +17,13 @@ export default function Home() {
     <section className={styles.main}>
       {fetched ? (
         <>
-          <h3 className={styles.heading}>{(yesVotes || 0) + (noVotes || 0)} personas ya votaron.</h3>
+          <span className={styles.heading}>{(yesVotes || 0) + (noVotes || 0)} personas ya votaron.</span>
         </>
-      ) : <span className={styles.loader}>Cargando datos...</span>}
+      ) : (
+        <span className={styles.loader}>
+          <Image src={`${process.env.NEXT_PUBLIC_BASE_PATH}/images/icons/preloader.svg`} alt="Cargando" width={32} height={32} />
+        </span>
+      )}
       <div>
         <QuizForm />
       </div>
