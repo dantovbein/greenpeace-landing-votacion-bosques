70cf352aa465b003b1b96da1119ebe72af8ee81c
Dan Tovbein <dtovbein@gmail.com>
2023-07-10 16:27:15 -0300

    Fix GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,11 @@ export default function RootLayout({
         <meta property="twitter:description" content={`${process.env.NEXT_PUBLIC_META_DESCRIPTION}`} />
         <meta property="twitter:image" content='https://votaporlosbosques.org/images/gp-bosques-header.jpg' />
         <meta property="twitter:image" content='https://votaporlosbosques.org/images/gp-bosques-header.jpg' />
-        <Script src="https://www.googletagmanager.com/gtag/js?id=GTM-TZB6GQ3" />
+        <Script>
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'Script','dataLayer','GTM-TZB6GQ3');
+          `}
+        </Script>
       </head>
       <body className={poppins.className}>
         <Script id="google-analytics">
